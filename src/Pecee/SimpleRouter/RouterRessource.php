a1271f1c895c8ba14e1958ef446820d79d75428b
Simon Sessingø <simon.sessingoe@gmail.com>
2015-10-07 13:26:41 +0200

    [BUGFIX] Fixed router not loading show method

--- src/Pecee/SimpleRouter/RouterRessource.php
+++ src/Pecee/SimpleRouter/RouterRessource.php
@@ -55,7 +55,7 @@ class RouterRessource extends RouterEntry {
         $url = parse_url($url);
         $url = $url['path'];
 
-        if(strtolower($url) == strtolower($this->url) || stripos($url, $this->url . '/') === 0) {
+        if(strtolower($url) == strtolower($this->url) || stripos($url, $this->url) === 0) {
             $url = rtrim($url, '/');
 
             $strippedUrl = trim(substr($url, strlen($this->url)), '/');
