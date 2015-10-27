bc163886134ffa73d96c1b3efd524c71d534b4ab
Simon Sessingø <simon.sessingoe@gmail.com>
2015-10-27 17:35:56 +0100

    [OPTIMISATION] Optimised middleware loading and routes.

--- src/Pecee/SimpleRouter/RouterEntry.php
+++ src/Pecee/SimpleRouter/RouterEntry.php
@@ -179,9 +179,9 @@ abstract class RouterEntry {
     public function getMergeableSettings() {
         $settings = $this->settings;
 
-        if(isset($settings['middleware'])) {
+        /*if(isset($settings['middleware'])) {
             unset($settings['middleware']);
-        }
+        }*/
 
         if(isset($settings['prefix'])) {
             unset($settings['prefix']);
