0be7bfcfd9418a517d0fa99da46fe3396f1f4e87
Simon Sessingø <simon.sessingoe@gmail.com>
2017-12-09 23:56:37 +0100

    Fixed utf-8 header issue (issue: #333)

--- src/Pecee/Http/Response.php
+++ src/Pecee/Http/Response.php
@@ -95,7 +95,7 @@ class Response
             throw new \InvalidArgumentException('Invalid type for parameter "value". Must be of type array or object implementing the \JsonSerializable interface.');
         }
 
-        $this->header('Content-type: application/json');
+        $this->header('Content-Type: application/json; charset=utf-8');
         echo json_encode($value, $options, $dept);
         exit(0);
     }
