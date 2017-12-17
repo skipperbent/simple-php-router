c1c25e19ff64cfd0e76c6f9cc00d5202fe77b3a0
Simon Sessingo <simon.sessingoe@gmail.com>
2017-12-17 10:38:13 +0100

    Enabled fallback _method for every request-type.

--- src/Pecee/Http/Request.php
+++ src/Pecee/Http/Request.php
@@ -18,7 +18,7 @@ class Request
         $this->host = $this->getHeader('http-host');
         $this->uri = $this->getHeader('request-uri');
         $this->input = new Input($this);
-        $this->method = strtolower($this->input->get('_method', $this->getHeader('request-method'), 'post'));
+        $this->method = strtolower($this->input->get('_method', $this->getHeader('request-method')));
     }
 
     protected function parseHeaders()
