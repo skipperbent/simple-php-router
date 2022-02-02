ea9a642a49811ce09818ca6641dc960f8ba8e6f5
DeveloperMarius <marius.karstedt@gmail.com>
2022-02-02 22:46:02 +0100

    removed ExceptionHandler debug message / phpunit fix

--- tests/Pecee/SimpleRouter/Dummy/Handler/ExceptionHandler.php
+++ tests/Pecee/SimpleRouter/Dummy/Handler/ExceptionHandler.php
@@ -4,7 +4,7 @@ class ExceptionHandler implements \Pecee\SimpleRouter\Handlers\IExceptionHandler
 {
 	public function handleError(\Pecee\Http\Request $request, \Exception $error)  : void
 	{
-	    echo $error->getMessage();
+	    //echo $error->getMessage();
 	}
 
 }
